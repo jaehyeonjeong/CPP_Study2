979ee0103e72dd0ef52d5deb09febf6bcaaf5f1c
definejae234 <definejae234@gmail.com>
2022-08-04 12:41:36 +0900

    Cpp3

---
+++ fstream.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	ofstream output;
+	output.open("test.txt");
+	output << "즐거운 프로그래밍!!!" << endl;
+	output.close();
+	return 0;
+}
